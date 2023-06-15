af33ce04062e8d402ef9babac3ad6f3c39dc0507
Said Arafat <s.arafat.selmi@gmail.com>
2023-06-15 23:20:04 +0300

    update car slice

--- src/store/slices/carSlice.js
+++ src/store/slices/carSlice.js
@@ -4,6 +4,18 @@ import axios from "axios";
 
 const apiEndpoint = `https://localhost:44315/api/Car`;
 
+const getToken = () => {
+  return localStorage.getItem("token");
+};
+
+axios.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export const fetchCars = createAsyncThunk(
   "cars/fetchCars",
   async (_, thunkAPI) => {
