c1d05366a4efe4f2b0b79d353351e464ac399183
Said Arafat <s.arafat.selmi@gmail.com>
2023-06-18 05:03:18 +0300

    Update home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,13 +2,17 @@ import React from "react";
 import CarList from "./car/carList";
 import GuestList from "./guest/guestList";
 import OwnerList from "./owner/ownerList";
+import UnitList from "./unit/unitList";
+import RoomList from "./room/roomList";
 
 const Home = () => {
   return (
     <div>
-      <CarList />
+      <RoomList />
       <GuestList />
+      <UnitList />
       <OwnerList />
+      <CarList />
     </div>
   );
 };
